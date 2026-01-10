8878560eb752eb3282db99963d8d491fe09574e0
Jallipalli Praveen <jallipallipraveen@gmail.com>
2026-01-10 16:52:37 +0530

    Updated the assignment

--- javascript-training/assignments.js
+++ javascript-training/assignments.js
@@ -1,1 +1,19 @@
 //Assignment
+let temparatureCity="25.5";
+console.log("Temperature of the city " +temparatureCity);
+
+let orderPlaced=true;
+console.log("Order status " +orderPlaced);
+
+let phoneNumber='123-456-7890';
+
+console.log("PhoneNumber " +phoneNumber);
+
+let accountBalance='1000.50';
+
+console.log("Account balance of the customer " +accountBalance);
+
+let emailAddress='jallipallipraveen@gmail.com';
+
+console.log("Email address of the person " +emailAddress);
+
