a58e7a540e7d52d1d12764e5399374143c686f22
Jallipalli Praveen <jallipallipraveen@gmail.com>
2026-07-28 19:06:24 +0530

    main commit

--- javascript-training/arrays.js
+++ javascript-training/arrays.js
@@ -1,12 +1,12 @@
 
-let arrays =["Employee","Job","salary","Tim","Kumar","Hen","Hike"];
+let arrays =["Employee","Job","salary","Tim","Kumar","Hen","Praveen"];
 
 
 console.log(arrays[3]);
 console.log(arrays[1]);
 console.log(arrays[2]);
 
-let arraysAndNumbers =["Employee",1,"Job",2,"salary",3,5,6];
+let arraysAndNumbers =["Employee",1,"Job",2,"salary",9,10,11];
 console.log(arraysAndNumbers[9]);
 console.log(arraysAndNumbers[3]);
 console.log(arraysAndNumbers[10]);
