eca5db13bd5830013de095689928b902f4c04ba8
Jallipalli Praveen <jallipallipraveen@gmail.com>
2026-07-28 19:04:40 +0530

    conflicts some

--- javascript-training/arrays.js
+++ javascript-training/arrays.js
@@ -1,12 +1,18 @@
 
-let arrays =["Employee","Job","salary","Tim","Kumar","Hen","Hike"];
+let arrays =["Employee","Job","salary","Tim","Kumar","Hen","Hike","Testing"];
 
 
 console.log(arrays[3]);
 console.log(arrays[1]);
 console.log(arrays[2]);
+console.log(arrays[3]);
+console.log(arrays[1]);
+console.log(arrays[2]);
+console.log(arrays[3]);
+console.log(arrays[1]);
+console.log(arrays[2]);
 
-let arraysAndNumbers =["Employee",1,"Job",2,"salary",3,5,6];
+let arraysAndNumbers =["Employee",1,"Job",2,"salary",3,5,6,"Praveen"];
 console.log(arraysAndNumbers[9]);
 console.log(arraysAndNumbers[3]);
 console.log(arraysAndNumbers[10]);
