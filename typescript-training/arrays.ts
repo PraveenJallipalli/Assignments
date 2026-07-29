1d8c8619ce2ecaf8e6198687248493338aab1caf
Jallipalli Praveen <jallipallipraveen@gmail.com>
2026-07-29 21:51:19 +0530

    merge conflicts

--- typescript-training/arrays.ts
+++ typescript-training/arrays.ts
@@ -1,7 +1,7 @@
 
 //testing Arrays
 let values:number[]= [1,2,3,4,5,6,7,8,9];
-let names:string[]=["apple", "banana","orange"];
+let names:string[]=["apple", "banana","orange","Camel"];
 
 // values.push(10);
 // console.log(values);
